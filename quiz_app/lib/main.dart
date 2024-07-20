75a059d8951a7789ce07ba6e3031ce97ead0c8b1
Faiza <faizanoor3001@gmail.com>
2024-07-20 23:26:55 +0300

    Added data model and data for questions app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -6,6 +6,8 @@ import 'package:quiz_app/quiz.dart';
 // is this an anti-pattern, what are the other ways or we generally don't pass any values from
 // the main.dart file level
 
-void main() {
-  runApp(const Quiz());
-}
+// if only single line of code in a function, can be written as arrow function
+void main() => runApp(const Quiz());
+// void main() {
+//   runApp(const Quiz());
+// }
