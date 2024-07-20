9283b0414d7ec3710e3d876dadc7415ee3d1080c
Mythra Varun <142224820+MythraVarun13@users.noreply.github.com>
2024-07-20 17:35:50 +0300

    Code cleanup

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -12,13 +12,6 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // Padding(
-          //   padding: const EdgeInsets.only(bottom: 20),
-          //   child: Image.asset(
-          //     'assets/images/quiz-logo.png',
-          //     width: 300,
-          //   ),
-          // ),
           Image.asset('assets/images/quiz-logo.png',
               width: 300, color: const Color.fromARGB(150, 255, 255, 255)),
           const SizedBox(
@@ -33,15 +26,11 @@ class StartScreen extends StatelessWidget {
           ),
           OutlinedButton.icon(
             onPressed: startQuiz,
-            // Styling can be done using the Button Style or the styleFrom
-            // function on the respective button types
-            // style: const ButtonStyle(
-            //   backgroundColor: WidgetStateColor.transparent,
-            // ),
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text(
               "Start Quiz",
+              // this will override the styling of the button
               // style: TextStyle(color: Colors.black),
             ),
           ),
